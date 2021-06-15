2b29130303cda7a5c8a1adb125a08cdde65df86e
sipandrei <pontos.serban@gmail.com>
2021-06-15 22:30:40 +0300

    afisare lista de teste si adaugare la alegere

--- main.js
+++ main.js
@@ -24,17 +24,12 @@ function schimbaTema() {
    darkModeButton.src = localStorage.getItem("theme-icon");
 }
 
-// functii trigonometrice inverse
-const form = document.querySelector(".formularRaspuns");
-const comanda = document.querySelector(".comanda");
-const raspuns = document.querySelector("#raspuns");
-const arataRaspuns = document.querySelector("#arata-raspunsul");
 let teste = [];
 scoatereTeste();
 
 function scoatereTeste() {
    if (localStorage.getItem("teste") != null)
-      JSON.parse(localStorage.getItem("teste"));
+      teste = JSON.parse(localStorage.getItem("teste"));
 }
 function setDate(nume, listaIntrebari) {
    this.nume = nume;
@@ -45,6 +40,90 @@ function intrebareRaspuns(intrebare, raspuns) {
    this.intrebare = intrebare;
    this.raspuns = raspuns;
 }
+
+// afisare seturi de intrebari si alegere
+const alegereSetIntrebari = document.querySelector("#alegereSetIntrebari");
+parcurgereTeste();
+
+function parcurgereTeste() {
+   for (let i = 0; i < teste.length; i++) afisareSeturiIntrebari(teste[i]);
+}
+
+function afisareSeturiIntrebari(test) {
+   const lista = alegereSetIntrebari.querySelector("form");
+   let radio = document.createElement("input");
+   radio.setAttribute("type", "radio");
+   radio.setAttribute("name", "set");
+   radio.setAttribute("id", `${test.nume}`);
+   lista.insertBefore(radio, lista.querySelector("#og"));
+   let label = document.createElement("label");
+   label.setAttribute("for", `${test.nume}`);
+   label.appendChild(document.createTextNode(`${test.nume}`));
+   lista.insertBefore(label, lista.querySelector("#og"));
+   lista.insertBefore(document.createElement("br"), lista.querySelector("#og"));
+}
+
+// TODO procesare alegere set date
+
+// adaugare set nou de date
+const numeNou = document.querySelector("#numeNou");
+const intrebareNoua = document.querySelector("#intrebareNoua");
+const raspunsNou = document.querySelector("#raspunsNou");
+const adaugareIntrebare = document.querySelector("#adaugareIntrebare");
+const setNou = document.querySelector("#setNou");
+
+let intrebariDeAdaugat = [];
+
+adaugareIntrebare.addEventListener("click", aditieLaCoada);
+setNou.addEventListener("submit", terminareSetNou);
+
+function afisareCoadaIntrebari() {
+   const coadaIntrebari = document.querySelector("#coadaIntrebari");
+   let li = document.createElement("li");
+   const ultimIndex = intrebariDeAdaugat.length - 1;
+   let liContent = `${intrebariDeAdaugat[ultimIndex].intrebare} - ${intrebariDeAdaugat[ultimIndex].raspuns}`;
+   li.appendChild(document.createTextNode(liContent));
+   coadaIntrebari.appendChild(li);
+}
+
+function aditieLaCoada() {
+   let intrebareSetNou = intrebareNoua.value;
+   let raspunsSetNou = raspunsNou.value;
+   if (intrebareSetNou != "" && raspunsSetNou != "") {
+      let aux = new intrebareRaspuns(intrebareSetNou, raspunsSetNou);
+      intrebariDeAdaugat.push(aux);
+      intrebareNoua.value = "";
+      raspunsNou.value = "";
+      afisareCoadaIntrebari();
+   } else alert("Trebuie sa adaugati o intrebare si un raspuns");
+}
+
+function stergereListaCoada(lista) {
+   while (lista.hasChildNodes()) {
+      lista.firstChild.remove();
+   }
+}
+
+function terminareSetNou(e) {
+   e.preventDefault();
+   if (intrebareNoua.value != "") aditieLaCoada();
+   if (intrebariDeAdaugat.length) {
+      aux = new setDate(numeNou.value, intrebariDeAdaugat);
+      intrebariDeAdaugat = [];
+      teste.push(aux);
+      const coadaIntrebari = document.querySelector("#coadaIntrebari");
+      stergereListaCoada(coadaIntrebari);
+      localStorage.setItem("teste", JSON.stringify(teste));
+      numeNou.value = "";
+      afisareSeturiIntrebari(aux);
+   } else alert("Nu se poate crea set de date fara intrebari");
+}
+
+// functii trigonometrice inverse
+const form = document.querySelector(".formularRaspuns");
+const comanda = document.querySelector(".comanda");
+const raspuns = document.querySelector("#raspuns");
+const arataRaspuns = document.querySelector("#arata-raspunsul");
 
 function creareFunctie(nume, domeniu, codomeniu, monotonie, paritate) {
    this.nume = nume;
@@ -113,57 +192,4 @@ function aratareRaspuns() {
    const containerRaspuns = document.querySelector("#container-raspuns");
    console.log(proprietateAleasa[0] + " : " + proprietateAleasa[1]);
    containerRaspuns.textContent = `Raspunsul corect pentru "${proprietateAleasa[0]}" este : ${proprietateAleasa[1]}`;
-}
-
-// adaugare set nou de date
-const numeNou = document.querySelector("#numeNou");
-const intrebareNoua = document.querySelector("#intrebareNoua");
-const raspunsNou = document.querySelector("#raspunsNou");
-const adaugareIntrebare = document.querySelector("#adaugareIntrebare");
-const setNou = document.querySelector("#setNou");
-
-let intrebariDeAdaugat = [];
-
-adaugareIntrebare.addEventListener("click", aditieLaCoada);
-setNou.addEventListener("submit", terminareSetNou);
-
-function afisareCoadaIntrebari() {
-   const coadaIntrebari = document.querySelector("#coadaIntrebari");
-   let li = document.createElement("li");
-   const ultimIndex = intrebariDeAdaugat.length - 1;
-   let liContent = `${intrebariDeAdaugat[ultimIndex].intrebare} - ${intrebariDeAdaugat[ultimIndex].raspuns}`;
-   li.appendChild(document.createTextNode(liContent));
-   coadaIntrebari.appendChild(li);
-}
-
-function aditieLaCoada() {
-   let intrebareSetNou = intrebareNoua.value;
-   let raspunsSetNou = raspunsNou.value;
-   if (intrebareSetNou != "" && raspunsSetNou != "") {
-      let aux = new intrebareRaspuns(intrebareSetNou, raspunsSetNou);
-      intrebariDeAdaugat.push(aux);
-      intrebareNoua.value = "";
-      raspunsNou.value = "";
-      afisareCoadaIntrebari();
-   } else alert("Trebuie sa adaugati o intrebare si un raspuns");
-}
-
-function stergereListaCoada(lista) {
-   while (lista.hasChildNodes()) {
-      lista.firstChild.remove();
-   }
-}
-
-function terminareSetNou(e) {
-   e.preventDefault();
-   if (intrebareNoua.value != "") aditieLaCoada();
-   if (intrebariDeAdaugat.length) {
-      aux = new setDate(numeNou.value, intrebariDeAdaugat);
-      intrebariDeAdaugat = [];
-      teste.push(aux);
-      const coadaIntrebari = document.querySelector("#coadaIntrebari");
-      stergereListaCoada(coadaIntrebari);
-      localStorage.setItem("teste", JSON.stringify(teste));
-      numeNou.value = "";
-   } else alert("Nu se poate crea set de date fara intrebari");
 }
